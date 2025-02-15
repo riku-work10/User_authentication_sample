9f7503d2177373075b6ebc872bebd4520a145dd3
riku-work10 <ukir19990530@gmail.com>
2025-02-15 22:35:16 +0900

    バックエンドの実装完了

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -8,9 +8,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:8000'
+    origins '*' # 本番環境では指定のドメインのみ許可
 
     resource "*",
       headers: :any,
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'], # 認証ヘッダー
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   mount ActionCable.server => '/ws'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -9,4 +10,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
 end
